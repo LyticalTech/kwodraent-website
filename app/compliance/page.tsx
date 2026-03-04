9cd92e10e25af9469185c0089894151c2175c824
Emmanuel Ezekiel <emamex44@gmail.com>
2026-03-04 12:58:13 +0100

    changes

--- app/compliance/page.tsx
+++ app/compliance/page.tsx
@@ -1,29 +1,13 @@
-// import Hero from './component/Hero';
-// import RulesSection from './component/RulesSection';
-// import Roles from '../subsidiaries/component/Roles';
-
-<<<<<<< HEAD
-// export default function Page() {
-// 	return (
-// 		<main className='w-full text-black'>
-// 			<Hero />
-// 			<RulesSection />
-// 			<Roles />
-// 		</main>
-// 	);
-// }
-=======
 import Hero from './component/Hero';
 import RulesSection from './component/RulesSection';
-import Description from '../who-we-are/component/Description';
-export default function () {
+import Roles from '../subsidiaries/component/Roles';
+
+export default function Page() {
 	return (
 		<main className='w-full text-black'>
 			<Hero />
 			<RulesSection />
-			<Description />
-			<CTASection />
+			<Roles />
 		</main>
 	);
 }
->>>>>>> 7f9dc62351e49f36793bc755d903bcbaa2b58140
